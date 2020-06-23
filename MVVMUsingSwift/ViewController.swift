190d9c55dd471584d47e8e26a163fdb592136097
Lucas Daniel Pedroso <lukas_boo@hotmail.com>
2020-06-23 10:30:49 -0300

    Initial Commit

---
+++ MVVMUsingSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MVVMUsingSwift
+//
+//  Created by Lucas Daniel on 23/06/20.
+//  Copyright © 2020 Lucas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
